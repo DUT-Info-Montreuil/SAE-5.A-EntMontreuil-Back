9c8bf625bf82178b2d884b269b1794e6ce609b13
PriyankSolanki <psolanki@condorcet93.fr>
2023-10-18 17:23:10 +0200

    password verification of user add

--- rest_api.py
+++ rest_api.py
@@ -9,6 +9,7 @@ from contextlib import closing
 from config import config
 import connect_pg
 import hashlib
+import re
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
@@ -39,47 +40,49 @@ def get_users():
 def add_users():
     try:
         jsonObject = request.json
-
         # Assurez-vous que "datas" est présent dans l'objet JSON
         if "datas" not in jsonObject:
             return jsonify({"message": "Missing 'datas' field in JSON"})
-
         data = jsonObject["datas"]
         password = data.get("password") 
         username = data.get("username")
-
         if not username:
             return jsonify({"message": "Missing 'username' field in JSON"})
-
         # Vérifiez si le nom d'utilisateur est déjà utilisé
         if username_exists(username):
             return jsonify({"message": "Username already in use"})
+
+        # Vérifiez le mot de passe
+        if len(password) < 12 : # plus de 12 caracteres
+            return jsonify({"message": "password need to contains minimum 12 characters"})
+        if not re.search(r'[A-Z]', password) : # au moins 1 majuscule
+            return jsonify({"message": "Password need to contains minimum 1 capital"})
+        if not re.search(r'[a-z]', password) : # au moins une minuscule
+            return jsonify({"message": "Password need to contains minimum 1 minuscule"})
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password) :  # Au moins un caractère spécial
+            return jsonify({"message": "Password need to contains minimum 1 special characters"})
+        if not re.search(r'[1-9]', password) : # au moins 1 chiffre
+            return jsonify({"message": "Password need to contains minimum 1 number"})
+
 
         hashed_password = hashlib.md5(password.encode()).hexdigest()
         # Créez la liste de colonnes et de valeurs
         columns = list(data.keys())
         values = list(data.values())
         values[columns.index("password")] = hashed_password
-
         # Établissez la connexion à la base de données
         conn = connect_pg.connect()
         cursor = conn.cursor()
-
         # Créez la requête SQL paramétrée
         query = f"INSERT INTO ent.users ({', '.join(columns)}) VALUES ({', '.join(['%s' for _ in columns])}) RETURNING id"
-
         # Exécutez la requête SQL avec les valeurs
         cursor.execute(query, values)
-
         # Récupérez l'identifiant de l'utilisateur inséré
         row = cursor.fetchone()
-
         # Validez la transaction et fermez la connexion
         conn.commit()
         conn.close()
-
         return jsonify({"message": "User added", "id": row[0]})
-
     except Exception as e:
         return jsonify({"message": "Error", "error": str(e)})
 

e6891670591152deb3efa42363c1d8a289e3d5be
ecyriaque <101125367+ecyriaque@users.noreply.github.com>
2023-11-02 17:12:49 +0100

    Add unit test for missing 'datas' key in POST request
    This test ensures that sending a POST request to the /trainings/add endpoint without the 'datas' key results in a 400 Bad Request response. The expected error message is 'Données manquantes', which is checked to be present in the JSON response. This validates that the endpoint correctly handles cases where the required data structure is not provided.

---
+++ tests/test_trainings.py
@@ -0,0 +1,22 @@
+import unittest
+import requests
+
+class AddTrainingTestCase(unittest.TestCase):
+    BASE_URL = 'http://localhost:5050'
+
+    def test_add_training_missing_datas_key(self):
+        # Send a POST request with a JSON body that doesn't include the 'datas' key
+        response = requests.post(
+            f'{self.BASE_URL}/trainings/add',
+            json={},  # An empty dict does not have the 'datas' key
+            headers={"Content-Type": "application/json"}
+        )
+
+        # The rest of the test is the same as before
+        self.assertEqual(response.status_code, 400, "Expected 400 Bad Request status code")
+        response_json = response.json()
+        self.assertIn('message', response_json, "JSON response does not contain 'message' key")
+        self.assertEqual(response_json['message'], "Données manquantes", "Expected error message 'Données manquantes' not found in the response")
+
+if __name__ == '__main__':
+    unittest.main()

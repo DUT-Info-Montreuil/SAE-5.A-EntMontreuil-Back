260b84a9e2ab10ba09e3137fbd59a777dee16887
ecyriaque <emilio.cyriaque97493@gmail.com>
2023-11-15 16:04:44 +0100

    Update Classroom model to include a list of materials
    Modified the ClassroomModel class to include a list of materials, where each material has an ID, a name, and a quantity. This change enhances the representation of classroom data and allows for a more comprehensive description of the classroom's available materials.

---
+++ services/classroom.py
@@ -0,0 +1,10 @@
+import psycopg2
+import connect_pg
+from entities.DTO.classroom import   Classroom
+from entities.model.classroomm import ClassroomModel
+
+class ClassroomService:
+    def __init__(self):
+        pass
+
+

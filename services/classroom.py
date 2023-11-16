54ccd9e4c791645f58529e613a45c172374fe6a8
ecyriaque <101125367+ecyriaque@users.noreply.github.com>
2023-11-16 22:52:12 +0100

    Add search and add equipment functionality
    This commit introduces two new features to the system:
    1. Search equipment: A new endpoint allows users to search for equipment based on various criteria such as equipment name, type, availability, etc. This enhances the usability of the system by providing a convenient way to find specific equipment.
    
    2. Add equipment to a classroom: Another endpoint enables users to add equipment to a classroom by specifying the classroom's ID and a list of equipment IDs to be added. This feature streamlines the process of equipping classrooms with the necessary resources.
    
    These enhancements improve the overall functionality and usability of the system, making it more versatile and efficient.

--- services/classroom.py
+++ services/classroom.py
@@ -83,3 +83,95 @@ class ClassroomService:
                 cursor.close()
             if conn is not None:
                 conn.close()
+
+    def search_classrooms(self, name=None, capacity=None, equipment=None, output_format="model"):
+        conn = None
+        cursor = None
+
+        try:
+            conn = connect_pg.connect()
+            cursor = conn.cursor()
+
+            # Build the query based on the provided criteria
+            query = "SELECT c.id, c.name, c.capacity, m.id AS material_id, m.equipment, cm.quantity FROM ent.Classroom c"
+            query += " LEFT JOIN ent.CONTAINS cm ON c.id = cm.id_classroom"
+            query += " LEFT JOIN ent.Materials m ON cm.id_materials = m.id"
+            conditions = []
+            values = []
+
+            if name:
+                conditions.append("c.name = %s")
+                values.append(name)
+            if capacity is not None:
+                conditions.append("c.capacity >= %s")
+                values.append(capacity)
+
+            if equipment:
+                conditions.append("m.equipment = %s")
+                values.append(equipment)
+
+            if conditions:
+                query += " WHERE " + " AND ".join(conditions)
+
+            cursor.execute(query, tuple(values))
+            rows = cursor.fetchall()
+
+            classroom_models = {}
+            for row in rows:
+                classroom_id = row[0]
+                if classroom_id not in classroom_models:
+                    classroom_model = ClassroomModel(
+                        id=row[0],
+                        name=row[1],
+                        capacity=row[2],
+                        materials=[]
+                    )
+                    classroom_models[classroom_id] = classroom_model
+
+                if row[3] is not None:
+                    material = {
+                        "id": row[3],
+                        "equipment": row[4],
+                        "quantity": row[5]
+                    }
+                    classroom_models[classroom_id].materials.append(material)
+
+            result = [classroom_model.jsonify() for classroom_model in classroom_models.values()]
+            return jsonify(result) if output_format == "model" else jsonify(result)
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
+        finally:
+            if cursor is not None:
+                cursor.close()
+            if conn is not None:
+                conn.close()
+
+    def add_equipments_to_classroom(self, id_Classroom, equipment_ids):
+            try:
+                conn = connect_pg.connect()  # Remplacez par votre fonction de connexion réelle
+                cursor = conn.cursor()
+
+                if not connect_pg.does_entry_exist("Classroom", id_Classroom):
+                    raise Exception("La salle de classe spécifiée n'existe pas.")
+                
+                if not equipment_ids:
+                    raise Exception("Aucun équipement spécifié à ajouter.")
+                    
+                # Vérifiez si les équipements existent et récupérez leurs IDs
+                cursor.execute("SELECT id FROM ent.Materials WHERE id IN %s", (tuple(equipment_ids),))
+                existing_equipment_ids = [row[0] for row in cursor.fetchall()]
+
+                if len(existing_equipment_ids) != len(equipment_ids):
+                    raise Exception("Certains équipements spécifiés n'existent pas.")
+
+                # Ajoutez les équipements à la salle de classe en utilisant les IDs
+                cursor.executemany("INSERT INTO ent.CONTAINS (id_classroom, id_materials, quantity) VALUES (%s, %s, %s)",
+                                    [(id_Classroom, equip_id, 1) for equip_id in existing_equipment_ids])
+
+                conn.commit()
+            except Exception as e:
+                conn.rollback()
+                raise e
+            finally:
+                cursor.close()
+                conn.close()

0d8c0b851a7abfc4c4fdbe34dc04a0839d18129b
PriyankSolanki <psolanki@condorcet93.fr>
2023-12-08 14:18:58 +0100

    search courses by week

--- controllers/Courses_controllers.py
+++ controllers/Courses_controllers.py
@@ -16,6 +16,11 @@ def get_course_by_id(course_id):
 def get_all_courses():
     return jsonify(course_service.get_all_courses().json)
 
+#get by promotion
+@courses_bp.route("/courses/promotion/<int:promotion_year>", methods=["GET"])
+def get_course_by_promotion(promotion_year):
+    return jsonify(course_service.get_course_by_promotion(promotion_year))
+
 #get by classroom
 @courses_bp.route("/courses/classroom/<string:classroom>", methods=["GET"])
 def get_course_by_classroom(classroom):
@@ -26,17 +31,13 @@ def get_course_by_classroom(classroom):
 def get_course_by_teacher(teacher_username):
     return jsonify(course_service.get_course_by_teacher(teacher_username))
 
-#get per day
-@courses_bp.route("/courses/day", methods=["GET"])
-def get_courses_by_day():
-    target_date = request.args.get("target_date")
-    return course_service.get_courses_by_day(target_date)
-
 #get by week
-@courses_bp.route("/courses/week", methods=["GET"])
-def get_courses_by_week():
-    target_week_start_date = request.args.get("target_week_start_date")
-    return course_service.get_courses_by_week(target_week_start_date)
+@courses_bp.route("/courses/week/<start_date>", methods=["GET"])
+def get_course_by_week(start_date):
+    reponse, http_status = course_service.get_course_by_week(start_date)
+    return reponse, http_status
+
+
 
 ##-----------------POST----------------
 @courses_bp.route("/courses", methods=["POST"])
@@ -69,4 +70,6 @@ def update_course(course_id):
 @courses_bp.route("/courses/<int:course_id>", methods=["DELETE"])
 def delete_course(course_id):
     return course_service.delete_course(course_id)
+
+
 
--- services/courses.py
+++ services/courses.py
@@ -2,9 +2,10 @@ import psycopg2
 import connect_pg
 from flask import jsonify
 from entities.model.coursesm import CourseModel
+from datetime import datetime, timedelta
 
 class CourseService:
-    
+    #------------------get by id-----------------------
     def get_course_by_id(self, course_id):
         try:
             conn = connect_pg.connect()
@@ -38,7 +39,7 @@ class CourseService:
         finally:
             conn.close()
 
-
+    #------------------get by classroom-----------------------
     def get_course_by_classroom(self, classroom):
         try:
             conn = connect_pg.connect()
@@ -71,8 +72,81 @@ class CourseService:
             return {"message": f"Erreur lors de la récupération du cours : {str(e)}"}, 500
         finally:
             conn.close()
+    #------------------get by promotion-----------------------
+    def get_course_by_promotion(self, promotion_year):
+        try:
+            conn = connect_pg.connect()
+            with conn.cursor() as cursor:
+                sql_query = """
+                   SELECT C.id, C.startTime, C.endTime, C.dateCourse, C.control, C.id_Resource, C.id_Tp, C.id_Td, C.id_Promotion, C.id_Teacher,
+                    C.id_classroom, C.id_Training, tr.name, tr.semester, R.name, TP.name, TD.name, P.year, P.level, T.initial, CL.name, R.color, 
+                    CL.capacity, U.username
+                    FROM ent.Courses C
+                    LEFT JOIN ent.Resources R ON C.id_Resource = R.id
+                    LEFT JOIN ent.TP TP ON C.id_Tp = TP.id
+                    LEFT JOIN ent.TD TD ON C.id_Td = TD.id
+                    LEFT JOIN ent.Promotions P ON C.id_Promotion = P.id
+                    LEFT JOIN ent.Trainings tr ON C.id_Training = tr.id
+                    LEFT JOIN ent.Teachers T ON C.id_Teacher = T.id
+                    LEFT JOIN ent.Classroom CL ON C.id_classroom = CL.id
+                    LEFT JOIN ent.Users U ON T.id_User = U.id
+                    WHERE P.year = %s
+                """
+
+                cursor.execute(sql_query, (promotion_year,))
+                row = cursor.fetchone()
+
+                if row:
+                    course_info = CourseModel(*row).jsonify()
+                    return course_info
+                else:
+                    return {"message": "Cours non trouvé"}, 404
+        except Exception as e:
+            return {"message": f"Erreur lors de la récupération du cours : {str(e)}"}, 500
+        finally:
+            conn.close()
+    #------------------get by week-----------------------
+    def get_course_by_week(self,start_date):
+        try:
+            conn = connect_pg.connect()
+            with conn.cursor() as cursor:
+                # Supposons que la date soit au format 'YYYY-MM-DD'
+                sql_query = '''
+                    SELECT C.id, C.startTime, C.endTime, C.dateCourse, C.control, C.id_Resource, C.id_Tp, C.id_Td, C.id_Promotion, C.id_Teacher,
+                            C.id_classroom, C.id_Training, tr.name, tr.semester, R.name, TP.name, TD.name, P.year, P.level, T.initial, CL.name, R.color, 
+                            CL.capacity, U.username
+                            FROM ent.Courses C
+                            LEFT JOIN ent.Resources R ON C.id_Resource = R.id
+                            LEFT JOIN ent.TP TP ON C.id_Tp = TP.id
+                            LEFT JOIN ent.TD TD ON C.id_Td = TD.id
+                            LEFT JOIN ent.Promotions P ON C.id_Promotion = P.id
+                            LEFT JOIN ent.Trainings tr ON C.id_Training = tr.id
+                            LEFT JOIN ent.Teachers T ON C.id_Teacher = T.id
+                            LEFT JOIN ent.Classroom CL ON C.id_classroom = CL.id
+                            LEFT JOIN ent.Users U ON T.id_User = U.id
+                    WHERE C.dateCourse >= %s AND C.dateCourse <= %s
+                '''
             
-            
+                start_date = datetime.strptime(start_date, '%Y-%m-%d')
+                end_date = start_date + timedelta(days=4)
+                
+                cursor.execute(sql_query, (start_date, end_date))
+                rows = cursor.fetchall()
+
+                if rows :
+                    courses_list = []
+
+                    for row in rows:
+                        course_info = CourseModel(*row)
+                        courses_list.append(course_info.jsonify())
+                    return {"courses": courses_list}, 404
+                else:
+                    return {"message": "Cours non trouvé"}, 404
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+        finally:
+            conn.close()
+    #------------------get by teacher-----------------------
     def get_course_by_teacher(self, teacher_username):
         try:
             conn = connect_pg.connect()
@@ -106,7 +180,7 @@ class CourseService:
         finally:
             conn.close()
             
-            
+    #------------------get all-----------------------
     def get_all_courses(self):
         try:
             conn = connect_pg.connect()
